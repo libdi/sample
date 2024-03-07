6974829ac1878adf68421498029344e8ab33c64d
Juyeong Maing <mjy9088@naver.com>
2024-03-07 11:09:05 +0900

    fix typo

--- src/component_cli.c
+++ src/component_cli.c
@@ -12,20 +12,19 @@ int main(int argc, char **argv) {
             argv[0]);
     exit(EXIT_FAILURE);
   }
-  di_internal_buffer_t data;
-  if (buffer_read(argv[1], &data))
+  buffer_t data;
+  if (di_internal_buffer_read(argv[1], &data))
     exit(EXIT_FAILURE);
-  di_internal_buffer_t function_body;
-  if (buffer_read(argv[2], &function_body))
+  buffer_t function_body;
+  if (di_internal_buffer_read(argv[2], &function_body))
     exit(EXIT_FAILURE);
-  di_internal_buffer_t initializer_function;
-  if (buffer_read(argv[3], &initializer_function))
+  buffer_t initializer_function;
+  if (di_internal_buffer_read(argv[3], &initializer_function))
     exit(EXIT_FAILURE);
-  di_internal_buffer_t finalizer_function;
-  if (buffer_read(argv[4], &finalizer_function))
+  buffer_t finalizer_function;
+  if (di_internal_buffer_read(argv[4], &finalizer_function))
     exit(EXIT_FAILURE);
-  di_internal_buffer_t dependency;
-  if (buffer_read(argv[5], &dependency))
+  buffer_t dependency;
+  if (di_internal_buffer_read(argv[5], &dependency))
     exit(EXIT_FAILURE);
-  // WIP: ...
 }
--- src/util.c
+++ src/util.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-err_t buffer_read(const char *file_name, buffer_t *out) {
+err_t di_internal_buffer_read(const char *file_name, buffer_t *out) {
   FILE *fp = fopen(file_name, "rb");
   if (fp == NULL)
     return true;
@@ -23,7 +23,7 @@ err_t buffer_read(const char *file_name, buffer_t *out) {
   return false;
 }
 
-err_t buffer_merge(size_t count, buffer_t *buffers, buffer_t *out) {
+err_t di_internal_buffer_merge(size_t count, buffer_t *buffers, buffer_t *out) {
   builder_t *result = di_internal_new_builder();
   if (!result)
     return true;
@@ -38,7 +38,7 @@ err_t buffer_merge(size_t count, buffer_t *buffers, buffer_t *out) {
   return false;
 }
 
-builder_t *new_builder(void) {
+builder_t *di_internal_new_builder(void) {
   builder_t *result = malloc(sizeof(builder_t));
   if (!result)
     return NULL;
@@ -64,14 +64,14 @@ static err_t preserve_capacity(builder_t *self, size_t to) {
   return false;
 }
 
-err_t builder_append(builder_t *self, buffer_t buffer) {
+err_t di_internal_builder_append(builder_t *self, buffer_t buffer) {
   if (preserve_capacity(self, self->length + buffer.length))
     return true;
   memcpy((char *)self->buffer + self->length, buffer.buffer, buffer.length);
   return false;
 }
 
-void builder_build(builder_t *self, buffer_t *out) {
+void di_internal_builder_build(builder_t *self, buffer_t *out) {
   out->buffer = self->buffer;
   out->length = self->length;
   self->buffer = NULL;
@@ -79,7 +79,7 @@ void builder_build(builder_t *self, buffer_t *out) {
   self->capacity = 0;
 }
 
-void delete_builder(builder_t *self) {
+void di_internal_delete_builder(builder_t *self) {
   free(self->buffer);
   free(self);
 }
